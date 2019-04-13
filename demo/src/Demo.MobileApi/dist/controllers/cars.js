c8e74132e1da54ffe12d468255026bb19eb5de37
Sander van Vliet <sander@codenizer.nl>
2019-04-13 17:08:48 +0200

    Remove dist folder

--- demo/src/Demo.MobileApi/dist/controllers/cars.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getCarById = function (req, res) {
-    res.status(200);
-    res.send("Hello car " + req.params.id + "!");
-};
--- demo/src/Demo.MobileApi/dist/controllers/users.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUserById = function (req, res) {
-    res.status(200);
-    res.send("Hello user" + req.params.id + "!");
-};
