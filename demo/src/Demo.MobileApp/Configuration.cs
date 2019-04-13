cfc6a6fa076278d537aa3fb1f28e03c784082ba5
Sander van Vliet <sander@codenizer.nl>
2019-04-13 17:19:47 +0200

    Setter should be public

--- demo/src/Demo.MobileApp/Configuration.cs
+++ demo/src/Demo.MobileApp/Configuration.cs
@@ -3,7 +3,7 @@ namespace Demo.MobileApp
     public class Configuration
     {
         public string MobileApiUrl { get; set; }
-        public string UserId { get; internal set; }
-        public string Vin { get; internal set; }
+        public string UserId { get; set; }
+        public string Vin { get; set; }
     }
 }
