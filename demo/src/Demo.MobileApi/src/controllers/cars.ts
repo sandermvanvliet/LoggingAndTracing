a11630758208de9afee9290d3dfed400eba02647
Sander van Vliet <sander@codenizer.nl>
2019-04-13 22:27:17 +0200

    Send back a proper json result from the controller for now

--- demo/src/Demo.MobileApi/src/controllers/cars.ts
+++ demo/src/Demo.MobileApi/src/controllers/cars.ts
@@ -2,5 +2,5 @@ import { Request, Response } from "express";
 
 export const getCarById = (req: Request, res: Response) => {
   res.status(200);
-  res.send("Hello car " + req.params.id + "!");
+  res.send("{\"message:\": \"Hello car " + req.params.id + "!\"}");
 };
--- demo/src/Demo.MobileApi/src/controllers/users.ts
+++ demo/src/Demo.MobileApi/src/controllers/users.ts
@@ -2,5 +2,5 @@ import { Request, Response } from "express";
 
 export const getUserById = (req: Request, res: Response) => {
   res.status(200);
-  res.send("Hello user " + req.params.id + "!");
+  res.send("{\"message:\": \"Hello user " + req.params.id + "!\"}");
 };
