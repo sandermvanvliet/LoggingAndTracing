9665f13b49e221a89c75836ef6ce5119af1d3f46
Sander van Vliet <sander@codenizer.nl>
2019-04-13 17:09:04 +0200

    Use port from env

--- demo/src/Demo.MobileApi/src/server.ts
+++ demo/src/Demo.MobileApi/src/server.ts
@@ -4,12 +4,17 @@ import * as usersController from "./controllers/users";
 import * as carsController from "./controllers/cars";
 
 const app = express();
-const port = 3000;
 
 app.use(bodyParser.json());
 app.set("port", process.env.PORT || 3000);
 
-app.get('/api/users/:id', (req, res) => usersController.getUserById(req, res));
-app.get('/api/cars/:id', (req, res) => carsController.getCarById(req, res))
+app.get("/api/users/:id", (req, res) => usersController.getUserById(req, res));
+app.get("/api/cars/:id", (req, res) => carsController.getCarById(req, res));
 
-app.listen(port, () => console.log(`Mobile API listening on port ${port}!`));
+app.listen(app.get("port"), () =>
+  console.log(
+    "  App is running at http://localhost:%d in %s mode",
+    app.get("port"),
+    app.get("env")
+  )
+);
