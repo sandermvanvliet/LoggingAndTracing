fb04a231b5a7f7047a09e7c225a170b145264358
Sander van Vliet <sander@codenizer.nl>
2019-04-14 09:24:18 +0200

    Call through to backend APIs

--- demo/src/Demo.MobileApi/src/controllers/cars.ts
+++ demo/src/Demo.MobileApi/src/controllers/cars.ts
@@ -1,6 +1,31 @@
 import { Request, Response } from "express";
+import { UrlOptions, CoreOptions } from "request";
+
+const defaultOptions = () => {
+  return {
+    url: process.env.CAR_API_URL,
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+};
 
 export const getCarById = (req: Request, res: Response) => {
-  res.status(200);
-  res.send("{\"message:\": \"Hello car " + req.params.id + "!\"}");
+  let options = defaultOptions();
+
+  options.url = `${options.url}/api/cars/${req.params.id}`;
+
+  let request = require("request");
+
+  request(<UrlOptions & CoreOptions>options, (error: any, response: Response, body: any) => {
+    if(error != null) {
+      console.log(error);
+    }
+    else {
+      res.status(response.statusCode);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(body);
+    }
+  });
 };
--- demo/src/Demo.MobileApi/src/controllers/users.ts
+++ demo/src/Demo.MobileApi/src/controllers/users.ts
@@ -1,6 +1,33 @@
 import { Request, Response } from "express";
+import { RequestCallback, UrlOptions, CoreOptions } from "request";
+
+const defaultOptions = () => {
+  return {
+    url: process.env.USER_API_URL,
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+};
 
 export const getUserById = (req: Request, res: Response) => {
-  res.status(200);
-  res.send("{\"message:\": \"Hello user " + req.params.id + "!\"}");
+  let options = defaultOptions();
+
+  options.url = `${options.url}/api/users/${req.params.id}`;
+
+  let request = require("request");
+
+  console.log(`Calling ${options.url}`);
+
+  request(<UrlOptions & CoreOptions>options, (error: any, response: Response, body: any) => {
+    if(error != null) {
+      console.log(error);
+    }
+    else {
+      res.status(response.statusCode);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(body);
+    }
+  });
 };
